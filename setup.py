0a541022885248e0d825e6a3427c9133f0f6c227
井上 顧基 <inoue.koki15@gmail.com>
2020-12-23 12:56:22 +0900

    update readme

--- setup.py
+++ setup.py
@@ -7,6 +7,18 @@ def load_requirements(f):
         filter(None,
                [l.split("#", 1)[0].strip() for l in open(os.path.join(os.getcwd(), f)).readlines()]))
 
+# README from README.md
+try:
+    from pypandoc import convert
+
+    def read_md(f):
+        return convert(f, 'rst')
+except ImportError:
+    convert = None
+    print("warning : Could not find module named pypandoc.")
+
+    def read_md(f):
+        return open(f, 'r').read()
 
 README = os.path.join(os.path.dirname(__file__), 'README.md')
 
@@ -14,7 +26,7 @@ setup(
     name="mylib",
     description="mylib",
     long_description=read_md(README),
-    version=VERSION,
+    version=0.0.1,
     install_requires=load_requirements("./requirements.txt"),
     packages=find_packages(exclude=('nobtebook'))
 )
